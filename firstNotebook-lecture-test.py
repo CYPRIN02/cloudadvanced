0dec7790cbeac442a919f10fe67ab2d2293a7f9f
CYPRIN02 <CYPRIN02>
2023-11-08 16:19:10 +0000

    first commit via databricks to github

--- firstNotebook-lecture-test.py
+++ firstNotebook-lecture-test.py
@@ -3,4 +3,4 @@ import pandas as pd
 # Lire le fichier CSV avec pandas
 df = spark.read.csv("/mnt/mount_grp7/data/test.csv",header=True)
 # Afficher les premières lignes du DataFrame
-display(df)
+display(df) 
